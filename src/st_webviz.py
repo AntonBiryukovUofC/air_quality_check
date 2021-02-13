c0c0eddaaea57aad0b93d39bc4d92ddfe8a69ee9
Anton Biryukov <anton.biryukov.ds@gmail.com>
2021-02-13 13:31:23 -0700

    added reqs for Heroku

--- src/st_webviz.py
+++ src/st_webviz.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import os
 import requests
+import altair as alt
+import numpy as np
+import pandas as pd
 from dotenv import find_dotenv, load_dotenv
-
-# load env variables
-
-load_dotenv(find_dotenv())
 
 
 def fetch_data_from_aq(city='Calgary', api_key=''):
@@ -16,13 +15,30 @@ def fetch_data_from_aq(city='Calgary', api_key=''):
     return data.json()
 
 
+# load env variables
+load_dotenv(find_dotenv())
+AQ_TOKEN = os.getenv('AQ_API_KEY', None)
+N_IMG = int(os.getenv('N_IMG', "10"))
+
+
+emoji_json = requests.get(url="https://api.github.com/emojis").json()
+img_source = pd.DataFrame({"name": list(emoji_json.keys()), "url": list(emoji_json.values()),
+                           'x': np.random.uniform(-50, 50, len(emoji_json)),
+                           'y': np.random.uniform(-50, 50, len(emoji_json))})
+img_subset =img_source.sample(N_IMG)
+ch = alt.Chart(img_subset,width=500,height=500).mark_image(width=20,height=20).encode(
+    x='x',
+    y='y',
+    url='url'
+)
+
+
 st.title('Air Quality check webapp')
 
 st.markdown('This app is a skeleton for what my SPE 2021 Data Science mentees will work on.'
             'Specifically, we would like to compare the air quality levels pre/post COVID in a year-over-year plot '
             'as a baseline')
 # get the api key
-qa_api_key = os.getenv('AQ_API_KEY', None)
-
-data = fetch_data_from_aq(city='Houston',api_key=qa_api_key)
+data = fetch_data_from_aq(city='Houston', api_key=AQ_TOKEN)
+st.altair_chart(ch)
 st.write(data)
