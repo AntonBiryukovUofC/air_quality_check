900f3fdf5883edb69a6a51a1071898f2a174ea99
acostalr <lr.acosta@outlook.com>
2021-03-22 15:57:56 -0600

    air quality webapp using InfluxDB

--- src/st_webviz_LA.py
+++ src/st_webviz_LA.py
@@ -7,96 +7,73 @@ import pandas as pd
 import os
 import psycopg2
 
-#DATABASE_URL = os.environ['DATABASE_URL']
-DATABASE_URL = 'postgres://zcnfhwcgogldfu:ddb669eda26b1ebba6acfae9b0be68da7de1254c7c48446c2181b0e017878e26@ec2-54-161-239-198.compute-1.amazonaws.com:5432/dbluphji0qouqq'
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+from datetime import datetime
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+from dotenv import find_dotenv, load_dotenv
+# You can generate a Token from the "Tokens Tab" in the UI
+load_dotenv(find_dotenv())
 
 
 def get_cities(conn):
-    """
-    Tranform a SELECT query into a pandas dataframe
-    """
-    cursor = conn.cursor()
-    try:
-        cursor.execute("select distinct city from aqdata order by city")
-    except (Exception, psycopg2.DatabaseError) as error:
-        print("Error: %s" % error)
-        cursor.close()
-        return 1
+    query_api = conn.query_api()
+    query= '''
+    from(bucket:"ts_spe")
+        |> range(start: 2019-01-01T23:30:00Z, stop: 2020-12-31T00:00:00Z)
+        |> filter(fn: (r) => r["_measurement"] == "luis-airquality")
+        |> group(columns:["City"])
+        |> distinct(column:"City")
+        |> keep(columns: ["_value"])
+        '''
+    df = query_api.query_data_frame(org=os.environ['INFLUX_ORG'], query=query)
+    return(df)
+
+
+
+def get_aq_data(conn,city,metric):
+    query_api = conn.query_api()
+    query= '''
+    from(bucket:"ts_spe")
+        |> range(start: 2019-01-01T23:30:00Z, stop: 2020-12-31T00:00:00Z)
+        |> filter(fn: (r) => r["_measurement"] == "luis-airquality")
+        |> filter(fn: (r) => r["City"] == "{selection}")
+        |> filter(fn: (r) => r["_field"] == "{field}")
+        |> yield(name: "mean")
+        '''.format(field=metric,selection=city)
     
-    # Naturally we get a list of tupples
-    tupples = cursor.fetchall()
-    cursor.close()
-    
-    # We just need to turn it into a pandas dataframe
-    df = pd.DataFrame(tupples, columns=['city'])
+    df = query_api.query_data_frame(org=os.environ['INFLUX_ORG'], query=query)
     return df
 
+# Connect to influxDB with AQ data
+influx_conn = InfluxDBClient(url=os.environ['INFLUX_HOST'], token=os.environ['INFLUX_TOKEN'])
 
-def get_air_data(conn,city,metric):
-    """
-    Tranform a SELECT query into a pandas dataframe
-    """
-    cursor = conn.cursor()
-    sql_statement = "Select date, {field} from aqdata where city = '{selection}' order by date".format(
-                field=metric,
-                selection=city)
-    try:
-        cursor.execute(sql_statement)
-    except (Exception, psycopg2.DatabaseError) as error:
-        print("Error: %s" % error)
-        cursor.close()
-        return 1
-    
-    # Naturally we get a list of tupples
-    tupples = cursor.fetchall()
-    cursor.close()
-    
-    # We just need to turn it into a pandas dataframe
-    df = pd.DataFrame(tupples, columns=['date',metric])
-    return df
 
-# Execute the "SELECT *" query
-df_cities = get_cities(conn)
-#df_cities.head()
 
-#url = 'https://raw.githubusercontent.com/AntonBiryukovUofC/air_quality_check/luis-testing/src/data/waqi-covid19-airqualitydata-filtered.csv'
-#df = pd.read_csv(url,sep=",")
-#cities = df.City.unique()
-#df_cities = pd.DataFrame(cities, columns=['City'])
-#df_cities = df_cities.sort_values(by=['City'])
-#df_cities = df_cities.reset_index(drop=True)
+# Obtain list of cities in DB
+df_cities = get_cities(influx_conn)
+# st.dataframe(df_cities)
 
+# User selection of one city to display
 option = st.selectbox(
     'Select City',
-     df_cities['city'])
+     df_cities['_value'])
 
 'You selected: ', option
 
+# # Obtain Air quality data of selected city
+df = get_aq_data(influx_conn,option,'no2')
+# st.dataframe(df.head(10))
 
-df = get_air_data(conn,option,'no2')
-
-#mask = df['City'] == option
-
-#df = postgresql_to_dataframe(conn, "Select date, no2 from aqdata where city = option order by date", column_names)
-
-#subset = df[mask]
-#subset = subset[["Date", "no2"]]
-#subset['Date']=pd.to_datetime(df['Date'])
-#subset = subset.set_index('Date')
-
-
+# # Display chart of selected city
 st.title('Air Quality check webapp')
 
 st.markdown('This app is a skeleton for what my SPE 2021 Data Science mentees will work on.'
             'Specifically, we would like to compare the air quality levels pre/post COVID in a year-over-year plot '
             'as a baseline')
 
-#st.dataframe(df)
 
 ch = alt.Chart(df).mark_line().encode(
-    x='date',
-    y='no2'
+    x='_time',
+    y='_value'
 )
 st.altair_chart(ch)
